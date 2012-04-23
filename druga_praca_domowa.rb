11f328ea20e7f4e2494d6ffa176059c12be09e3d
Tomasz Milczarek <tomasz.milczarek@gmail.com>
2012-04-24 01:58:25 +0200

    Param added to second task

--- druga_praca_domowa.rb
+++ druga_praca_domowa.rb
@@ -2,24 +2,24 @@ require 'net/smtp'
 
 class DrugaPracaDomowa
   def first_task
-    Array.new(6) { |i| i*i }
+     Array.new(6) { |i| i*i }
   end
 
   def second_task
-	deciphered = "grgdm#5#gr#8#l#srgqlhv#gr#f}zduwhm#srwhjl".each_byte { |e| putc e-3 }
-	((2+5)**4).to_s.each_byte.collect { |e| (e+3).chr }.join
+    deciphered = "grgdm#5#gr#8#l#srgqlhv#gr#f}zduwhm#srwhjl".each_byte { |e| putc e-3 }
+    ((2+5)**4).to_s.each_byte.collect { |e| (e+3).chr }.join
   end
 
-  def third_task
-    IO.read("pracadomowa.txt")
+  def third_task file_name = "pracadomowa.txt"
+    IO.read(file_name)
   end
 
   def fourth_task
     smtp = Net::SMTP.start('your.smtp.server', 25)
     if smtp.method(:check_auth_args).arity == 2
-    	smtp.check_auth_args "user", "pass"
+      smtp.check_auth_args "user", "pass"
     else
-    	smtp.check_auth_args "user", "pass", :auth_type
+      smtp.check_auth_args "user", "pass", :auth_type
     end
   end
-end	
+end 
